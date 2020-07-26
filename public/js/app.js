987a20bd3f0131176f01c6ac851e4fcf60c8c57f
Süleyman Ekmekçi <suleymanekmekci3@gmail.com>
2020-07-26 18:26:47 +0300

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-
-
 const weatherForm = document.querySelector('form');
 // takes value from search
 const search = document.querySelector('input');
@@ -17,7 +13,7 @@ weatherForm.addEventListener('submit', (event) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 
         response.json().then((data) => {
 
